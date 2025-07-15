8d0932abfa64a2f56affb23dc9ec5e331e96f3cf
Venu Pagadala <venugopalreddypagadala@gmail.com>
2025-07-14 22:34:05 -0400

    updated mobile view

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,13 +1,11 @@
-import GitHubIcon from '@mui/icons-material/GitHub';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import '../assets/styles/Main.scss';
-import profilePic from './../assets/images/profile.jpeg';
-import EmailIcon from '@mui/icons-material/Email';  // Add this import
-import './Main.scss'
-
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import "../assets/styles/Main.scss";
+import profilePic from "./../assets/images/profile.jpeg";
+import EmailIcon from "@mui/icons-material/Email"; // Add this import
+import "./Main.scss";
 
 function Main() {
-
   return (
     <div className="container">
       <div className="about-section">
@@ -16,18 +14,45 @@ function Main() {
         </div>
         <div className="content">
           <div className="social_icons">
-            <a href="https://github.com/venupagadala" target="_blank" rel="noreferrer"><GitHubIcon/></a>
-            <a href="https://www.linkedin.com/in/venu-pagadala-77ab3a251/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+            <a
+              href="https://github.com/venupagadala"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <GitHubIcon />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/venu-pagadala-77ab3a251/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <LinkedInIcon />
+            </a>
             <a href="mailto:venupagadala13@gmail.com">
-          <EmailIcon />
-        </a>
+              <EmailIcon />
+            </a>
           </div>
           <h1>Venu Pagadala</h1>
           <h2>Frontend Developer</h2>
 
           <div className="mobile_social_icons">
-            <a href="https://github.com/yujisatojr" target="_blank" rel="noreferrer"><GitHubIcon/></a>
-            <a href="https://www.linkedin.com/in/yujisato/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+            <a
+              href="https://github.com/yujisatojr"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <GitHubIcon />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/yujisato/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <LinkedInIcon />
+            </a>
+             <a href="mailto:venupagadala13@gmail.com">
+              <EmailIcon />
+            </a>
           </div>
         </div>
       </div>
